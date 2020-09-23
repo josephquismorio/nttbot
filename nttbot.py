3ebdaa3048667bbeee38f938263f7560eb39e1ff
joey quismorio <70463608+josephquismorio@users.noreply.github.com>
2020-09-23 12:39:24 -0500

    Add files via upload

--- nttbot.py
+++ nttbot.py
@@ -1,11 +1,13 @@
 import discord
 import os
+import praw
 
 from discord.ext import commands
 
 from dotenv import load_dotenv
 
 #reads .env file for user discord token
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -15,9 +17,10 @@ bot = commands.Bot(command_prefix = '!', case_insensitive = True)
 #confirm ntt bot works
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("suh dude simulator"))
     print(f'{bot.user} successfully booted.')
     bot.load_extension('cogs.fun')
     bot.load_extension('cogs.music')
-
+    bot.load_extension('cogs.quiplash')
 #to do: implement spotipy compatibility; other bs
 bot.run(TOKEN)
